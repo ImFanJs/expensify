ca70bb6ce9e30cff1238e2d3403466bbd95f6591
Jesus Castañeda <>
2017-11-06 21:20:23 -0600

    Adding redux to expensify app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
+
 import 'normalize.css/normalize.css';
 import './styles/styles.scss'
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const store = configureStore();
+
+store.dispatch(addExpense({description: 'Water bill', amount: 200, createdAt: 2000}));
+store.dispatch(addExpense({description: 'Gas bill', amount: 100, createdAt: 1000}));
+
+store.dispatch(setTextFilter('water'));
+
+setTimeout(() => {
+    store.dispatch(setTextFilter('bill'));
+}, 3000);
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+
+console.log(visibleExpenses);
+
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import ExpenseList from './ExpenseList';
+import ExpenseListFilters from './ExpenseListFilters';
 import {Link} from 'react-router-dom';
 
 const ExpenseDashboardPage = () => (
     <div>
-        This my expense dashboard component.
+        <ExpenseListFilters />
+        <ExpenseList />
         <Link to='/edit/12'>Item 12</Link>
         <Link to='/edit/13'>Item 13</Link>
     </div>
