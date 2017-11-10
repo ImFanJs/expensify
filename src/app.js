61626a4b8767adf80d65d005b1087ba91bb4b6cd
Jesus Castañeda <>
2017-11-09 22:48:01 -0600

    Adding add expense funcionality

--- src/app.js
+++ src/app.js
@@ -16,16 +16,8 @@ store.dispatch(addExpense({description: 'Water bill', amount: 200, createdAt: 20
 store.dispatch(addExpense({description: 'Gas bill', amount: 100, createdAt: 1000}));
 store.dispatch(addExpense({description: 'Rent', amount: 150000, createdAt: 500}));
 
-store.dispatch(setTextFilter('water'));
-
-setTimeout(() => {
-    store.dispatch(setTextFilter('bill'));
-}, 3000);
-
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-
-console.log(visibleExpenses);
 
 const jsx = (
     <Provider store={store}>
